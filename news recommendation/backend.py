26617de3170a8f1ef45baf4987475a5f71b7186e
Revuubot <aayusha766@gmail.com>
2025-01-15 00:54:41 +0530

    Update backend.py

--- news recommendation/backend.py
+++ news recommendation/backend.py
@@ -16,3 +16,11 @@ def recommend():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route("/form")
+def login():
+    return"<html><head><title>Register</title></head><body><h1>Register</h1><form method="post" action="/register"><input type="text" name="username" placeholder="Username" required><input type="password" name="password" placeholder="Password" required> <button type="submit">Register</button></form></body></html>"
+
+
+
+
